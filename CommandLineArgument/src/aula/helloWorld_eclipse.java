41f3cd82b6a802e5bab573e8b16c14b08b77db39
RenanCatini <renan.amaral@sou.unifal-mg.edu.br>
2025-03-13 11:56:19 -0300

    Atualização na pasta Java

---
+++ CommandLineArgument/src/aula/helloWorld_eclipse.java
@@ -0,0 +1,30 @@
+package aula;	//Sempre a primeira opcao é package ou import
+
+class helloWorld_eclipse {
+	
+	//Primeiro metodo sempre vai se chamar main
+	public static void main(String[] args) {
+		System.out.println("Hello Cruel Eclipse World!");
+		System.out.println(args.length);
+	}
+
+}
+
+/*
+ * Classe:
+ * Objeto:
+ * 
+ * Classe é um substantivo, nenhuma classe é verbo
+ * 
+ * psvn + tab : gera a estrutura basica Main do Java (intellj)
+ * main + ctrl + space : gera a estrutura basica Main do Java (eclipse)
+ * 
+ * sysout + ctrl + space : System.out.println();
+ * 
+ * public : modificador de acesso que permite qualquer comando acessar
+ * privado : somente dentro do metodo 
+ * 
+ * static : o metodo Main pertence apenas a classe helloWorld_eclipse.
+ * 
+ * (String[] args) : pode passar uma string como parametro
+ */
---
+++ PassagemDeParametros/PassagemParametros.java
@@ -0,0 +1,11 @@
+
+class PassagemParametros {
+
+   public static void main(String[] nome) {
+	if(nome.length != 0)
+	   for(String s: nome)
+		System.out.println(s);
+	else 
+	   System.out.println("Vazio");
+   }
+}
